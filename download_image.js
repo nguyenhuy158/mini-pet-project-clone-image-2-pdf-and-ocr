1b11d63ad6321459dc7295d1e9d46c975cc6dd75
Nguyen Huy <ntqhuy2k2@gmail.com>
2023-07-06 10:14:24 +0700

    done task: regex to output, ouput to image

---
+++ download_image.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+const fs = require("fs");
+const readline = require("readline");
+
+const inputFile = "output.txt";
+const prefix = "chapter2/";
+
+if (!fs.existsSync(prefix)) {
+  fs.mkdirSync(prefix);
+  console.log(`Created directory ${prefix}`);
+} else {
+  console.log(`Directory ${prefix} already exists`);
+}
+
+const rl = readline.createInterface({
+  input: fs.createReadStream(inputFile),
+  crlfDelay: Infinity,
+});
+
+let lineNumber = 1;
+
+rl.on("line", (url) => {
+  const outputFile = prefix + `${lineNumber}.jpg`;
+  axios({
+    url,
+    responseType: "stream",
+  })
+    .then((response) => {
+      response.data.pipe(fs.createWriteStream(outputFile)).on("close", () => {
+        console.log(`Saved image to ${outputFile}`);
+      });
+    })
+    .catch((error) => {
+      console.log(`Error downloading image: ${error}`);
+    });
+  lineNumber++;
+});
---
+++ regex.js
@@ -0,0 +1,32 @@
+// const regex = /data:image\/jpg;base64,([^"';,\s]+)/g;
+// const regex = /\"(.+?)\"/g;
+
+const fs = require("fs");
+const readline = require("readline");
+
+// Tạo interface để đọc dữ liệu từ tệp tin
+const rl = readline.createInterface({
+  input: fs.createReadStream("input.txt"),
+  crlfDelay: Infinity,
+});
+
+// Biểu thức chính quy để tìm kiếm các chuỗi khớp
+const regex = /data:image\/jpg;base64,([^"';,\s]+)/g;
+
+// Mảng để lưu các chuỗi khớp
+const matches = [];
+
+// Đọc từng dòng trong tệp tin
+rl.on("line", (line) => {
+  // Tìm kiếm các chuỗi khớp và thêm vào mảng matches
+  const match = line.match(regex);
+  if (match) {
+    matches.push(match[0]);
+  }
+});
+
+// Khi đã đọc xong tệp tin, ghi kết quả vào tệp tin output.txt
+rl.on("close", () => {
+  fs.writeFileSync("output.txt", matches.join("\n"));
+  console.log("Đã lưu kết quả vào tệp tin output.txt");
+});
