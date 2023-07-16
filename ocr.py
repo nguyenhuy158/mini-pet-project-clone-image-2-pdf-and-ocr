351636856aec437063b1bf16a00ead234e1c4e8f
Nguyen Huy <ntqhuy2k2@gmail.com>
2023-07-16 21:51:20 +0700

    update ocr

--- ocr.py
+++ ocr.py
@@ -2,11 +2,15 @@ import pytesseract
 from PIL import Image
 import os
 
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
 output_file = 'chapter2.txt'
 prefix = 'chapter2/'
+output_file = 'chapter4.txt'
+prefix = 'chapter4/'
 
 with open(output_file, 'w') as f:
-    for i in range(1, 51):
+    for i in range(1, 38):
         filename = prefix + f'{i}.jpg'
         img = Image.open(filename)
         text = pytesseract.image_to_string(img, lang='eng')
